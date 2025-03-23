012b1def8f427332eac73ce049bb879497eaafc1
FloridaSuka <155165730+FloridaSuka@users.noreply.github.com>
2025-03-23 22:46:15 +0100

    Update TestDb.java

--- demo/src/main/java/KNK_2025/TestDb.java
+++ demo/src/main/java/KNK_2025/TestDb.java
@@ -3,20 +3,11 @@ package KNK_2025;
 import Database.DBConnector;
 import java.sql.*;
 
-//Krijimi i lidhjes me bazen e te dhenave
-//Ekzekutimi i pyetesoreve nga kodi
 public class TestDb {
     public static void main(String[] args)  throws SQLException {
-//        String url = "jdbc:postgresql://localhost/Knk-2025";
-//        String user = "postgres";
-//        String password = "leonna123";
-//        Connection connection = DriverManager.getConnection(url,user,password);
-//        if(connection.isValid(100)){
-//            System.out.println("DB connected");
         Connection connection = DBConnector.getConnection();
-        String query = " INSERT INTO Users(Id, Emri, Mbiemri, Mosha, Email ) VALUES (401, 'filann','filannii', 25,'fila.n@gmail.com')";
+        String query = " INSERT INTO Users(Id, Emri, Mbiemri, Mosha, Email ) VALUES (101, 'filann','filannii', 25,'fila.n@gmail.com')";
         Statement statement = connection.createStatement();
-        //statement.executeQuery();
         statement.execute(query);
         statement.execute("DELETE FROM users WHERE id = 1");
         query = " SELECT * FROM Users ";
@@ -34,5 +25,7 @@ public class TestDb {
             System.out.println("Age: " + age);
             System.out.println("------------------");
         }
+        //fshirja e te gjitha rekordeve
+        statement.execute("TRUNCATE TABLE Users RESTART IDENTITY");
     }
 }
