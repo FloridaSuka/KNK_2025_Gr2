3c17813665467a37abe32a6e6ea47a2363f67e37
FloridaSuka <155165730+FloridaSuka@users.noreply.github.com>
2025-04-04 12:19:38 +0200

    models

---
+++ demo/src/main/java/models/dto/Nxenesit.java
@@ -0,0 +1,44 @@
+package models;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Nxenesit {
+    private int id;
+    private String name;
+    private String name;
+    private String email;
+    
+    private int age;
+
+    private User(int id, String name, String email, int age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public static User getInstance(ResultSet result) throws SQLException {
+        int id = result.getInt("id");
+        String name = result.getString("name");
+        String email = result.getString("email");
+        int age = result.getInt("age");
+        return new User(id, name, email, age);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
