9abdaefe5f40d3cfd23af3cab5dc485cc1b52b54
FloridaSuka <155165730+FloridaSuka@users.noreply.github.com>
2025-04-04 12:35:56 +0200

    ndryshim

--- demo/src/main/java/models/Nxenesit.java
+++ demo/src/main/java/models/Nxenesit.java
@@ -6,23 +6,31 @@ import java.sql.SQLException;
 public class Nxenesit {
     private int id;
     private String name;
-    private String name;
+    private String surname;
     private String email;
-    
     private int age;
+    private char  gjinia[];
+    private String adresa[];
+    private String phone;
 
-    private User(int id, String name, String email, int age) {
+    private User(int id, String name, String email, String surname int age, String[] adresa, String phone, char[]  gjinia) {
         this.id = id;
-        this.name = name;
+        this.emri = emri;
+        this.mbiemri = mbiemri;
         this.email = email;
-        this.age = age;
+        this.mosha = mosha;
+        this.gjinia = gjinia;
+        this.adresa = adresa;
+        this.phone = phone;
     }
 
     public static User getInstance(ResultSet result) throws SQLException {
         int id = result.getInt("id");
-        String name = result.getString("name");
+        String emri = result.getString("emri");
+        String mbiemri=result.getString("mbiemri");
         String email = result.getString("email");
-        int age = result.getInt("age");
+        int mosha = result.getInt("mosha");
+        String adresa[]
         return new User(id, name, email, age);
     }
 
