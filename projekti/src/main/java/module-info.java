e589205d5af6dd4ebbaa8c518e0d013d9e80a667
gresadaqi <gresa.daqi11@gmail.com>
2025-04-27 18:23:51 +0200

    Update module-info.java

--- projekti/src/main/java/module-info.java
+++ projekti/src/main/java/module-info.java
@@ -6,8 +6,10 @@ module com.example.demo {
     opens com.example.demo to javafx.fxml;
     opens database to javafx.fxml;
     opens knk2025 to javafx.fxml;
+    opens controllers to javafx.fxml;
 
     exports com.example.demo;
+    exports controllers;
     exports database;
     exports knk2025;
 }
