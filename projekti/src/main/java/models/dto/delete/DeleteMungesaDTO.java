d5bd618d3a12fadb3d3a3a903448beb732136fb7
gresadaqi <gresa.daqi11@gmail.com>
2025-05-05 21:55:33 +0200

    Create DeletMungesaDTO

---
+++ projekti/src/main/java/models/dto/delete/DeleteMungesaDTO.java
@@ -0,0 +1,13 @@
+package models.dto.delete;
+
+public class DeleteMungesaDTO {
+    private int id;
+
+    public DeleteMungesaDTO(int id) {
+        this.id = id;
+    }
+
+    public int getId() { return id; }
+
+    public void setId(int id) { this.id = id; }
+}
