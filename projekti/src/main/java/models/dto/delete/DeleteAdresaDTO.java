a3d1f8b9ac49b4d60f8acf45695fc278fdf2b83b
diellakika <diella.kika@student.uni-pr.edu>
2025-05-05 17:56:29 +0200

    AdresaDTO

---
+++ projekti/src/main/java/models/dto/delete/DeleteAdresaDTO.java
@@ -0,0 +1,15 @@
+package models.dto.delete;
+
+
+public class DeleteAdresaDTO {
+    private int id;
+
+    public DeleteAdresaDTO(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
+
