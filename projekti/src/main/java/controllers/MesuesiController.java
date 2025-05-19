7f27c281d4d92f2923d2dcfc40781f062420ea21
Leona Zullufi <leonazullufi14@gmail.com>
2025-05-20 01:16:01 +0200

    Lidhja e butonave me View (mesuesiView)

--- projekti/src/main/java/controllers/MesuesiController.java
+++ projekti/src/main/java/controllers/MesuesiController.java
@@ -16,51 +16,22 @@ import java.io.IOException;
 
 public class MesuesiController {
 
-//    @FXML
-//    private void btnNxenesi(ActionEvent event) {
-//        loadFXML(event, "/views/nxenesiView.fxml");
-//    }
-//
-//    @FXML
-//    private void btnNota(ActionEvent event) {
-//        loadFXML(event, "/views/MenaxhimiINotave.fxml");
-//    }
-//
-//    @FXML
-//    private void btnStatistikat(ActionEvent event) {
-//        loadFXML(event, "/views/StatistikaView.fxml");
-//    }
-//
-//    @FXML
-//    private void btnMungesat(ActionEvent event) {
-//        loadFXML(event, "/views/MenaxhimiMungesave.fxml");
-//    }
-//
-//    @FXML
-//    private void btnProfile(ActionEvent event) {
-//        loadFXML(event, "/views/ProfileView.fxml");
-//    }
-//
-//    @FXML
-//    private void btnSignOut(ActionEvent event) {
-//        loadFXML(event, "/views/login.fxml");
-//    }
-//
-//    private void loadFXML(ActionEvent event, String fxmlPath) {
-//        try {
-//            Parent root = FXMLLoader.load(getClass().getResource(fxmlPath));
-//            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-//            stage.setScene(new Scene(root));
-//            stage.show();
-//        } catch (IOException e) {
-//            System.out.println("❌ Nuk u gjet skedari: " + fxmlPath);
-//            e.printStackTrace();
-//        }
-//    }
     @FXML private MenuButton menuLanguage;
 
     @FXML public void initialize() {
         LanguageHandler.configureLanguageMenu(menuLanguage, SceneLocator.TEACHER_PAGE);
+    }
+    @FXML
+    private void handleStudents(ActionEvent event) {
+        SceneNavigator.switchScene((Node) event.getSource(), SceneLocator.STUDENT_PAGE);
+    }
+    @FXML
+    private void handleGrades(ActionEvent event) {
+        SceneNavigator.switchScene((Node) event.getSource(), SceneLocator.GRADE_MANAGEMENT_PAGE);
+    }
+    @FXML
+    private void handleAbsence(ActionEvent event) {
+        SceneNavigator.switchScene((Node) event.getSource(), SceneLocator.ABSENCES_PAGE);
     }
     @FXML
     private void handleLogout(ActionEvent event) {
