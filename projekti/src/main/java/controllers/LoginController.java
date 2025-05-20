d667fef8c3e50780c1c8a0fa93039899ad8caa97
FloridaSuka <155165730+FloridaSuka@users.noreply.github.com>
2025-05-20 04:23:57 +0200

    db

--- projekti/src/main/java/controllers/LoginController.java
+++ projekti/src/main/java/controllers/LoginController.java
@@ -75,7 +75,6 @@ public class LoginController {
             switch (u.getRole()) {
                 case ADMIN:
                     System.out.println("🔄 Duke kaluar në Admin View...");
-
                     SceneNavigator.switchScene(source, ADMIN_PAGE);
                     break;
 
