36e9ad86755ec3f71d3ca7d9ebb3c28b1755dcd9
gresadaqi <gresa.daqi11@gmail.com>
2025-05-21 23:36:01 +0200

    lenda menubar

--- projekti/src/main/java/controllers/LendaController.java
+++ projekti/src/main/java/controllers/LendaController.java
@@ -1,12 +1,12 @@
 package controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import models.dto.create.CreateLenda;
 import repositories.LendaRepository;
 import utils.LanguageHandler;
+import utils.MenuUtils;
 import utils.SceneLocator;
 
 public class LendaController {
@@ -24,6 +24,62 @@ public class LendaController {
     @FXML
     public void initialize() {
         LanguageHandler.configureLanguageMenu(menuLanguage, SceneLocator.SUBJECT_MANAGEMENT_PAGE);
+        String name = this.getClass().getSimpleName();
+        System.out.println("🔍 Controller aktiv: " + name);
+        MenuUtils.populateOpenSubMenu(menuOpen, name);
+    }
+    @FXML private MenuItem menuCut, menuCopy, menuPaste, menuUndo, menuSelectAll, menuRedo;
+    @FXML private Menu menuOpen;
+
+    @FXML
+    public void handleNew(ActionEvent event) {
+        MenuUtils.handleNew();
+    }
+
+    @FXML
+    public void handleOpen() {
+        // Shembull: ky controller është për admin
+        MenuUtils.openConditionalView("MenaxhimiDrejtoreveController", "menaxhimiDrejtoreve.fxml", "Menaxhimi i Drejtoreve");
+    }
+
+    @FXML
+    public void handleQuit() {
+        System.exit(0);
+    }
+
+    @FXML
+    public void handleUndo() {
+        MenuUtils.performUndo(menuUndo.getParentPopup().getOwnerWindow().getScene());
+    }
+
+    @FXML
+    public void handleRedo() {
+        MenuUtils.performRedo(menuRedo.getParentPopup().getOwnerWindow().getScene());
+    }
+
+    @FXML
+    public void handleCut() {
+        MenuUtils.performCut(menuCut.getParentPopup().getOwnerWindow().getScene());
+    }
+
+    @FXML
+    public void handleCopy() {
+        MenuUtils.performCopy(menuCopy.getParentPopup().getOwnerWindow().getScene());
+    }
+
+    @FXML
+    public void handlePaste() {
+        MenuUtils.performPaste(menuPaste.getParentPopup().getOwnerWindow().getScene());
+    }
+
+    @FXML
+    public void handleSelectAll() {
+        MenuUtils.performSelectAll(menuSelectAll.getParentPopup().getOwnerWindow().getScene());
+    }
+
+    @FXML
+    public void handleHelp() {
+        MenuUtils.openhelp();
     }
     @FXML
     private void shtoLenda() {
