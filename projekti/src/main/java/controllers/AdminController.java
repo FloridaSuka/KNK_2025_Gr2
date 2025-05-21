8d1f528753f17d0da9e5fb3dc1850b2fdfb597b4
gresadaqi <gresa.daqi11@gmail.com>
2025-05-21 23:41:06 +0200

    ndryshim

--- projekti/src/main/java/controllers/AdminController.java
+++ projekti/src/main/java/controllers/AdminController.java
@@ -3,11 +3,8 @@ package controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.Menu;
 import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
 import utils.LanguageHandler;
-import utils.MenuUtils;
 import utils.SceneLocator;
 import utils.SceneNavigator;
 
@@ -17,69 +14,12 @@ public class AdminController {
 
     @FXML
     private MenuButton menuLanguage;
-    @FXML private MenuItem menuCut, menuCopy, menuPaste, menuUndo, menuSelectAll, menuRedo;
-    @FXML private Menu menuOpen;
+
 
     @FXML
     public void initialize() {
-        String name = this.getClass().getSimpleName();
-        System.out.println("🔍 Controller aktiv: " + name);
-        MenuUtils.populateOpenSubMenu(menuOpen, name);
         LanguageHandler.configureLanguageMenu(menuLanguage, SceneLocator.ADMIN_PAGE);
     }
-
-    @FXML
-    public void handleNew(ActionEvent event) {
-        MenuUtils.handleNew();
-    }
-
-    @FXML
-    public void handleOpen() {
-        // Shembull: ky controller është për admin
-        MenuUtils.openConditionalView("MenaxhimiDrejtoreveController", "menaxhimiDrejtoreve.fxml", "Menaxhimi i Drejtoreve");
-    }
-
-    @FXML
-    public void handleQuit() {
-        System.exit(0);
-    }
-
-    @FXML
-    public void handleUndo() {
-        MenuUtils.performUndo(menuUndo.getParentPopup().getOwnerWindow().getScene());
-    }
-
-    @FXML
-    public void handleRedo() {
-        MenuUtils.performRedo(menuRedo.getParentPopup().getOwnerWindow().getScene());
-    }
-
-    @FXML
-    public void handleCut() {
-        MenuUtils.performCut(menuCut.getParentPopup().getOwnerWindow().getScene());
-    }
-
-    @FXML
-    public void handleCopy() {
-        MenuUtils.performCopy(menuCopy.getParentPopup().getOwnerWindow().getScene());
-    }
-
-    @FXML
-    public void handlePaste() {
-        MenuUtils.performPaste(menuPaste.getParentPopup().getOwnerWindow().getScene());
-    }
-
-    @FXML
-    public void handleSelectAll() {
-        MenuUtils.performSelectAll(menuSelectAll.getParentPopup().getOwnerWindow().getScene());
-    }
-
-    @FXML
-    public void handleHelp() {
-        MenuUtils.openhelp();
-    }
-
-
     @FXML
     private void onOpenSettings(ActionEvent event) {
         SceneNavigator.switchScene((Node) event.getSource(), SceneLocator.SETTINGS_PAGE);
