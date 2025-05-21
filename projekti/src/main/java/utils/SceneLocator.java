0ba0797372121dac6732ca413e889612a82daff4
Leona Zullufi <leonazullufi14@gmail.com>
2025-05-21 20:35:49 +0200

    .

--- projekti/src/main/java/utils/SceneLocator.java
+++ projekti/src/main/java/utils/SceneLocator.java
@@ -33,6 +33,8 @@ public class SceneLocator {
     public final static String PRINCIPAL_MANAGEMENT_PAGE = "/views/MenaxhimiDrejtoreve.fxml";
     public final static String PRINCIPAL_STATISTICS_PAGE = "/views/StatistikatDrejtor.fxml";
     public static final String STUDENT_ABSENCES_PAGE = "/views/Mungesat.fxml";
+    public final static String TEACHER_STATISTICS_PAGE = "/views/statistikaMesuesi.fxml";
+
 
     public static void locate(Event event, String form) {
         Node eventNode = (Node) event.getSource();
